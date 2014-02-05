bfd75781aa3ef3fb1e352a5020f6788eabc1f70e
Shahmeer Navid <shahmeernavid@gmail.com>
2014-02-05 17:16:32 -0500

    gjhghjgj

--- views/home.js
+++ views/home.js
@@ -1,13 +1,14 @@
 AppNamespace.home = function(){
     var viewModel ={
-        message: ko.observable('Welcome!'),
+        message: "Hello World!"
+        /*message: ko.observable('Welcome!'),
         name: ko.observable(''),
         sayHello: function(){
             this.message("Hello " + this.name() + '!');
         },
         viewMain: function(){
             AppNamespace.app.navigate("main");
-        }
+        }*/
     };
     return viewModel;
 }
