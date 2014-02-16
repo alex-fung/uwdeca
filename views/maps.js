88aa53c9c7f3447b12c515597de93e0693d3f909
Alex Fung <alexcfung4000@gmail.com>
2014-02-16 17:18:05 -0500

    Added NavBar for maps, plus map widget

--- views/maps.js
+++ views/maps.js
@@ -1,5 +1,23 @@
 AppNamespace.maps = function(){
     var viewModel = {
+    	details: ko.observable(),
+    	navItemClicked: function (e) {
+    		this.details(e.itemData.contents);
+    		if(e.itemData.text != "UW Campus")
+    			$('#map').hide();
+		   	else
+		   		$('#map').show();
+		}
     };
+
     return viewModel;
 }
+
+navItems = [
+	{ text: "UW Campus", icon: "find", contents:"" },
+    { text: "QNC", icon: "user", contents:"<strong>QNC</strong>" },
+    { text: "Waterloo Inn", icon: "find", contents:"<strong>Waterloo</strong>" }
+];
+
+
+
