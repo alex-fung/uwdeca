337a00e66bf6d93746c55988bfa2c2537acd1616
Alex Fung <alexcfung4000@gmail.com>
2014-02-16 19:51:13 -0500

    Allowed for selection of locations in the map

--- views/maps.js
+++ views/maps.js
@@ -1,23 +1,45 @@
 AppNamespace.maps = function(){
     var viewModel = {
     	details: ko.observable(),
+    	locationItemClicked: function(e) {
+    		this.visible(false);
+    		$('#map').dxMap('instance').removeMarker(this.markers);
+    		$('#map').dxMap('instance').addMarker(e.itemData);
+    		if(e.itemData.text == "Quantum Nano Centre")
+    			$('#map').dxMap('instance').option('zoom', 19);
+    		else if(e.itemData.text == "Waterloo Inn")
+    			$('#map').dxMap('instance').option('zoom', 15);
+    		else
+    			$('#map').dxMap('instance').option('zoom', 18);
+    	},
     	navItemClicked: function (e) {
     		this.details(e.itemData.contents);
-    		if(e.itemData.text != "UW Campus")
+    		if(e.itemData.text != "UW Campus") {
     			$('#map').hide();
-		   	else
+    			$('#locations').hide();
+    		}
+		   	else {
 		   		$('#map').show();
-		}
+		   		$('#locations').show();
+		   	}
+		},
+		visible: ko.observable(false),
+		togglePopover: function() {
+		    this.visible(!this.visible());
+		},
+		markers: [
+			{ location: [43.47212, -80.543961], text: "Quantum Nano Centre" }, 
+			{ location: [43.491413, -80.529356], text: "Waterloo Inn" }, 
+			{ location: [43.465955, -80.522404], text: "Beta Night Club" }
+		],
+		navItems: [
+			{ text: "UW Campus", icon: "find", contents:"" },
+		    { text: "QNC", icon: "user", contents:"<strong>QNC</strong>" },
+		    { text: "Waterloo Inn", icon: "find", contents:"<strong>Waterloo</strong>" }
+		]
     };
 
     return viewModel;
 }
-
-navItems = [
-	{ text: "UW Campus", icon: "find", contents:"" },
-    { text: "QNC", icon: "user", contents:"<strong>QNC</strong>" },
-    { text: "Waterloo Inn", icon: "find", contents:"<strong>Waterloo</strong>" }
-];
-
 
 
