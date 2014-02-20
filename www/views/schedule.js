343be27a02b8e3e491e6e265dff390421be77726
Shahmeer Navid <shahmeernavid@gmail.com>
2014-02-20 17:06:28 -0500

    conference schedule

--- www/views/schedule.js
+++ www/views/schedule.js
@@ -12,29 +12,6 @@ x.searchExpr(function(dataItem) {
     return dataItem.name;
 });
 
-$.ajax({ 
-        url:"http://syntaxdonors.org/uwdeca/data.json", 
-        dataType: "json",
-        async: false,
-        error: function(jqXHR, textStatus, errorThrown){
-            showError("Network Error");
-        }
-        
-
-        }).done( function (data) {
-
-                    for (var i = 0; i < data.length; i++) {
-                        x.store().insert(data[i]);
-                    }
-
-
-                    //usefull if we want to group stuff, read docs for more info - dont need it right now
-                    //x.group(function (dataItem) {
-                    //    return dataItem.name.charAt(0);
-                    //});
-        });
-
-
 
 function search(query){
     x.searchValue(query);
@@ -71,7 +48,33 @@ function navigateToDelegate(){
 
 
 AppNamespace.personal_schedule = function(){
+    $.ajax({ 
+        url:"http://syntaxdonors.org/uwdeca/data.json", 
+        dataType: "json",
+        async: false,
+        error: function(jqXHR, textStatus, errorThrown){
+            showError("Network Error");
+        }
+        
+
+        }).done( function (data) {
+
+                    for (var i = 0; i < data.length; i++) {
+                        x.store().insert(data[i]);
+                    }
+
+
+                    //usefull if we want to group stuff, read docs for more info - dont need it right now
+                    //x.group(function (dataItem) {
+                    //    return dataItem.name.charAt(0);
+                    //});
+        });
+
+        
     var viewModel = {
+
+
+
         text: ko.observable(""),
         test: function(){
             search(viewModel.text());
