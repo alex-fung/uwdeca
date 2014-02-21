8ea8f71831cffbef9c4e634c87f01357c645a975
Shahmeer Navid <shahmeernavid@gmail.com>
2014-02-20 21:45:29 -0500

    -

--- platforms/ios/www/views/main.js
+++ platforms/ios/www/views/main.js
@@ -1,30 +1,42 @@
-function getCurrentEvent(){
-    var schedule = getSchedule();
-    var rightNow = new Date();
-
-    for (var i = 0; i < schedule.length; i++) {
-        var start = new Date(schedule[i].startTime);
-        var end = new Date(schedule[i].endTime);
-
-
-
-        if(start.getTime() < rightNow.getTime() && end.getTime() > rightNow.getTime()) return schedule[i];
-
-    }
-
-    return -1;
-
-}
-
 AppNamespace.main = function(){
     var viewModel = {
+        getSchedule: function (){
+            var response = undefined;
+            $.ajax({ 
+                url:"http://syntaxdonors.org/uwdeca/schedule.json", 
+                dataType: "json",
+                async: false,
+                error: function(jqXHR, textStatus, errorThrown){
+                    showError("Network Error");
+                }}).done(function(data){
+                    response = data;
+                });
+            return response;
+        },
+        getCurrentEvent: function (){
+            var schedule = viewModel.getSchedule();
+            var rightNow = new Date();
+
+            for (var i = 0; i < schedule.length; i++) {
+                var start = new Date(schedule[i].startTime);
+                var end = new Date(schedule[i].endTime);
+
+
+
+                if(start.getTime() < rightNow.getTime() && end.getTime() > rightNow.getTime()) return schedule[i];
+
+            }
+
+            return -1;
+
+        },
         showUpdate: false,
         eventTitle: ko.observable("No Current Event"),
         eventStart: ko.observable("N/A"),
         eventEnd: ko.observable("N/A"),
         eventLocation: ko.observable("N/A"),
         update: function(){
-            var currentEvent = getCurrentEvent();
+            var currentEvent = viewModel.getCurrentEvent();
             var noEvent = (currentEvent === -1);
             if(viewModel.eventTitle() === ((noEvent)? "No Current Event": currentEvent.title) && 
                 viewModel.eventStart() === ((noEvent)? "N/A":formatDate(currentEvent.startTime)) && 
